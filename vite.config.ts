2848cd8fd7e4a9a9b088571245491144b3466ebb
VineVax <git@vinevax.net>
2024-05-26 20:27:44 +0200

    Update base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import viteBasicSslPlugin from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     vue(),
   ],
