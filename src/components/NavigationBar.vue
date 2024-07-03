f46c50b1045d6f24f7587fba4c5e59e7f5d47c05
VineVax <git@vinevax.net>
2024-07-03 10:51:36 +0200

    Add navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  modelValue: String,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const updateValue = (newValue: any) => {
+  emit('update:modelValue', newValue);
+};
+
+const classes = (value: string) => {
+  if (props.modelValue === value) {
+    return 'border-white text-white';
+  }
+
+  return 'border-transparent text-gray-500 hover:border-gray-500 hover:text-gray-300';
+}
+</script>
+
+<template>
+  <div class="bg-indigo-950">
+    <div class="border-b border-black">
+      <nav class="-mb-px flex">
+        <button
+            @click="updateValue('current')"
+            class="w-1/3 border-b-2 px-1 py-4 text-center text-sm font-medium"
+            v-bind:class="classes('current')"
+        >
+          Current
+        </button>
+        <button
+            @click="updateValue('planning')"
+            class="w-1/3 border-b-2 px-1 py-4 text-center text-sm font-medium"
+            v-bind:class="classes('planning')"
+        >
+          Planning
+        </button>
+        <button
+            @click="updateValue('completed')"
+            class="w-1/3 border-b-2 px-1 py-4 text-center text-sm font-medium"
+            v-bind:class="classes('completed')"
+        >
+          Completed
+        </button>
+      </nav>
+    </div>
+  </div>
+</template>
--- src/views/ExtensionIndex.vue
+++ src/views/ExtensionIndex.vue
@@ -4,6 +4,7 @@ import {useAniList} from "@/services/aniList";
 import {ref} from "vue";
 import AnimeCard from "@/components/AnimeCard.vue";
 import type {MediaListEntry} from "@/types";
+import NavigationBar from "@/components/NavigationBar.vue";
 
 const { getCurrentWatching } = useAniList();
 
@@ -14,14 +15,18 @@ window.Twitch.ext.configuration.onChanged(async () => {
 });
 
 const list = ref<MediaListEntry[]|null>(null);
+
+const currentNavigation = ref('current');
 </script>
 
 <template>
-  <div v-if="list" class="bg-indigo-950/50 px-4">
-    <h1 class="text-2xl text-white text-center py-3">Anime Watchlist</h1>
-    <div class="grid grid-cols-2 gap-4">
-      <div v-for="anime in list" v-bind:key="anime.media.id">
-        <anime-card :anime="anime" />
+  <div v-if="list" class="bg-indigo-950/50">
+    <navigation-bar v-model="currentNavigation" />
+    <div class="px-1.5">
+      <div class="grid grid-cols-3 gap-4 mt-4">
+        <div v-for="anime in list" v-bind:key="anime.media.id">
+          <anime-card :anime="anime" />
+        </div>
       </div>
     </div>
   </div>
